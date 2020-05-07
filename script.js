28419792b5c526974f119bc3cdb498a2f997d820
Jk <jmiller55890@gmail.com>
2020-05-07 10:49:00 -0400

    Added individual rolls to script

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
-var roll = "";
-const clickedDice = document.querySelector(".diceTray");
- clickedDice.addEventListener('click', () => {
- var roll= Math.floor(Math.random()*101);
- console.log(roll);
- });
+var roll = ""; //contents of top-box (current role)
+
+
+const clickedDice = document.querySelectorAll(".dice");
+clickedDice.forEach((dice) => {
+    dice.addEventListener('click',(e) =>{
+    
+        switch(event.target.id){ //individual dice rolls
+            
+            case "d20":
+             var roll= Math.floor(Math.random()*20+1);  
+            document.getElementById("currentRollP").innerText = roll;
+                break;   
+            
+            case "d12":
+             var roll= Math.floor(Math.random()*12+1);  
+             document.getElementById("currentRollP").innerText = roll;
+                break;
+
+            case "d10":
+              var roll= Math.floor(Math.random()*10+1);  
+              document.getElementById("currentRollP").innerText = roll;
+                break;
+            
+            case "d100":
+              var roll= Math.floor(Math.random()*100+1);  
+              document.getElementById("currentRollP").innerText = roll;
+                break;
+
+            case "d6":
+               var roll= Math.floor(Math.random()*6+1);  
+               document.getElementById("currentRollP").innerText = roll;
+                break;
+
+             case "d4":
+                var roll= Math.floor(Math.random()*4+1);  
+                document.getElementById("currentRollP").innerText = roll;
+                  break;
+    }        
+    });
+    
+ 
+});
